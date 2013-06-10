3f3c356004afc8653f16063b4f8b28c462873517
George Dra <sonicgd@gmail.com>
2013-06-10 14:37:46 +0600

    rename xhprof name

--- www/index-test.php
+++ www/index-test.php
@@ -19,7 +19,7 @@ require_once($yii);
 Yii::createWebApplication($config)->run();
 
 if (extension_loaded('xhprof')) {
-    $profiler_namespace = 'denis'; // namespace for your application
+    $profiler_namespace = 'meteroryiit'; // namespace for your application
     $xhprof_data = xhprof_disable();
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
