4b024b58258ab91078b63372874b35591cbe33cf
George Dra <sonicgd@gmail.com>
2013-06-10 14:36:10 +0600

    Call xhprof in index-test

--- www/index-test.php
+++ www/index-test.php
@@ -3,13 +3,24 @@
  * This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
  */
-
+if (extension_loaded('xhprof')) {
+    include_once 'tools/xhprof_lib/utils/xhprof_lib.php';
+    include_once 'tools/xhprof_lib/utils/xhprof_runs.php';
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+}
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/test.php';
+$yii = dirname(__FILE__) . '/../framework/yii.php';
+$config = dirname(__FILE__) . '/protected/config/test.php';
 
 // remove the following line when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
+
+if (extension_loaded('xhprof')) {
+    $profiler_namespace = 'denis'; // namespace for your application
+    $xhprof_data = xhprof_disable();
+    $xhprof_runs = new XHProfRuns_Default();
+    $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
+}
